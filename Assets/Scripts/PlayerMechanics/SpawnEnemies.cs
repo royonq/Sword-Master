3fdc0350ea7c9df800dd050ca710f6fce9a5caec
MrD1mple <vanoroy1403@gmail.com>
2024-09-30 20:11:54 +0200

    refactor changed spawn logic

--- Assets/Scripts/PlayerMechanics/SpawnEnemies.cs
+++ Assets/Scripts/PlayerMechanics/SpawnEnemies.cs
@@ -1,18 +1,15 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
     [SerializeField] private GameObject _enemy;
     [SerializeField] private Transform _playerPosition;
-    [SerializeField] float _spawnRadius;
+    [SerializeField] private float _spawnRadius;
     [SerializeField] private float _spawnRate;
-    [SerializeField] private FollowTarget _followTarget;
 
     private void Start()
     {
-        _followTarget.Target = _playerPosition;
         StartCoroutine(SpawnRate());
     }
     private IEnumerator SpawnRate()
@@ -21,7 +18,9 @@ public class SpawnEnemies : MonoBehaviour
         {
             yield return new WaitForSeconds(_spawnRate);
             Vector3 _spawnOffset = new Vector2(Random.Range(-_spawnRadius, _spawnRadius), Random.Range(-_spawnRadius, _spawnRadius));
-            Instantiate(_enemy, _playerPosition.position + _spawnOffset, _playerPosition.rotation);
+            GameObject newEnemy = Instantiate(_enemy, _playerPosition.position + _spawnOffset, _playerPosition.rotation);
+            FollowTarget _followTarget = newEnemy.GetComponent<FollowTarget>();
+            _followTarget.Target = _playerPosition;
         }
     }
 }

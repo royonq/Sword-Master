1323482d3114419f8797198e4c49f3374ea7b1c2
MrD1mple <vanoroy1403@gmail.com>
2024-11-09 19:19:33 +0100

    refactored movement system swich from old to new input system

--- Assets/Scripts/Input/PlayerInput.cs
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -4,9 +4,16 @@ public class PlayerInput : MonoBehaviour
 {
     [SerializeField] private PausePanel _pausePannel;
     private UseSatilateSword _useAbitities;
+    private PlayerMovement _playerMovement;
     private void Start()
     {
         _useAbitities = GetComponent<UseSatilateSword>();
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        _playerMovement.MoveDirection = context.ReadValue<Vector2>();
     }
 
     public void OnUseAbility(InputAction.CallbackContext context)

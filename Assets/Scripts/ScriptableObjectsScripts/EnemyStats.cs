9068c02df4255a5f94ac2269e6febf1ca525b0bd
MrD1mple <vanoroy1403@gmail.com>
2024-12-17 19:30:02 +0100

    refactor scriptable objects

--- Assets/Scripts/ScriptableObjectsScripts/EnemyStats.cs
+++ Assets/Scripts/ScriptableObjectsScripts/EnemyStats.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewEnemy", menuName = "Enemy")]
 public class EnemyStats : MobStats
 {
-    [SerializeField] private MobStats _mobStats;
-    public MobStats MobStats { get { return _mobStats; } }
-
-
     [SerializeField] private float _killExpirience;
     public float KillExpirience { get { return _killExpirience; } }
 }
--- Assets/Scripts/ScriptableObjectsScripts/PlayerStats.cs
+++ Assets/Scripts/ScriptableObjectsScripts/PlayerStats.cs
@@ -2,10 +2,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPlayer", menuName = "Player")]
 public class PlayerStats : MobStats
 {
-    [SerializeField] private MobStats _mobStats;
-    public MobStats MobStats { get { return _mobStats; } }
-
-
     [SerializeField] private float _moneyCount;
     public float MoneyCount { get { return _moneyCount; } }
 }

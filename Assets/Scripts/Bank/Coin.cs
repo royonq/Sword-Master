82e5853eba57c6d5ae191d5169eea7939e02ef6c
Luxi4ka <vaivanto1403@gmail.com>
2025-04-04 17:57:39 +0200

    refactor Wallet Scripts

--- Assets/Scripts/Bank/Coin.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Coin : MonoBehaviour
-{
-    [SerializeField] private int _cost;
-    public int Cost { get { return _cost; } }
-}
--- Assets/Scripts/ScriptableObjectsScripts/WalletDatabase.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(fileName = "NewWallet", menuName = "Data/EarnItems/Wallet")]
-public class WalletDatabase : ScriptableObject
-{
-    [SerializeField] private int _moneyCount;
-    public int MoneyCount => _moneyCount;
-}

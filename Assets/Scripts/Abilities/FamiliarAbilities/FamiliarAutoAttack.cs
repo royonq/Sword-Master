875ac070616955e5098c15cb5ea9de1dac73fa0a
Luxi4ka <vaivanto1403@gmail.com>
2025-05-03 18:32:10 +0200

    refactor

--- Assets/Scripts/Abilities/FamiliarAbilities/FamiliarAutoAttack.cs
+++ Assets/Scripts/Abilities/FamiliarAbilities/FamiliarAutoAttack.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 public class FamiliarAutoAttack : MonoBehaviour, IFamiliarAbility
 {
     [SerializeField] private float _damage;
-    private readonly List<GameObject> _targetsInRange = new();
-
+    private readonly List<Enemy> _targetsInRange = new();
+    
     public void Use()
     {
         _targetsInRange.RemoveAll(enemy => !enemy);
-
         foreach (var enemy in _targetsInRange)
         {
-            enemy.GetComponent<Enemy>().TakeDamage(_damage);
+            enemy.TakeDamage(_damage);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Enemy") && !_targetsInRange.Contains(other.gameObject))
+        if (other.CompareTag("Enemy"))
         {
-            _targetsInRange.Add(other.gameObject);
+            var enemy = other.GetComponent<Enemy>();
+            if (!_targetsInRange.Contains(enemy))
+            {
+                _targetsInRange.Add(enemy);
+            }
         }
     }
 }

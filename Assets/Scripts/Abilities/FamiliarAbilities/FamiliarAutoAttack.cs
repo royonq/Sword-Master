2e8c66879edc65823ded4c79e6f386d2e207b802
Luxi4ka <vaivanto1403@gmail.com>
2025-05-05 10:15:44 +0200

    added familiar ability scriptable balance

--- Assets/Scripts/Abilities/FamiliarAbilities/FamiliarAutoAttack.cs
+++ Assets/Scripts/Abilities/FamiliarAbilities/FamiliarAutoAttack.cs
@@ -3,9 +3,27 @@ using UnityEngine;
 
 public class FamiliarAutoAttack : MonoBehaviour, IFamiliarAbility
 {
-    [SerializeField] private float _damage;
+    [SerializeField] private FamiliarAutoatackData _familiarAutoatackData;
+
     private readonly List<Enemy> _targetsInRange = new();
-    
+    private BoxCollider2D _boxCollider2D;
+
+    private float _damage;
+    private float _abilityRadius;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _damage = _familiarAutoatackData.Damage;
+        _abilityRadius = _familiarAutoatackData.AbilityRadius;
+        _boxCollider2D = GetComponent<BoxCollider2D>();
+        _boxCollider2D.size = new Vector2(_abilityRadius, _abilityRadius);
+    }
+
     public void Use()
     {
         foreach (var enemy in _targetsInRange)

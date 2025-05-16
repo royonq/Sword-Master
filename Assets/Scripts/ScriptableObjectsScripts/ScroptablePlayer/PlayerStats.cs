b5dbec69e6950b30655e2b35648082e7fe15e7ff
Luxi4ka <vaivanto1403@gmail.com>
2025-05-16 18:09:48 +0200

    refactor

--- Assets/Scripts/ScriptableObjectsScripts/ScroptablePlayer/PlayerStats.cs
+++ Assets/Scripts/ScriptableObjectsScripts/ScroptablePlayer/PlayerStats.cs
@@ -4,6 +4,4 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewPlayer", menuName = "Data/Damageble/Player")]
 public class PlayerStats : MobStats
 {
-    [SerializeField] private SoundType _ultimateSound;
-    public SoundType UltimateSound => _ultimateSound;
 }

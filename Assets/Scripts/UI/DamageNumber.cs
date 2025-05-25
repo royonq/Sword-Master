2195811009c5162c98f3c0f4ab0bb6c01947088e
Luxi4ka <vaivanto1403@gmail.com>
2025-05-25 20:27:00 +0200

    added pool objects system

--- Assets/Scripts/UI/DamageNumber.cs
+++ Assets/Scripts/UI/DamageNumber.cs
@@ -5,39 +5,33 @@ using TMPro;
 public class DamageNumber : MonoBehaviour
 {
     [SerializeField] private DamageNumberStats _damageNumberStats;
-    
-    private  float _moveUpSpeed;
-    private  float _lifeTime;
-    
+
+    private float _moveUpSpeed;
+    private float _lifeTime;
     private TextMeshPro _text;
 
-    private void Awake()
-    {
-        Init();
-        StartCoroutine(MoveAndDestroy());
-    }
-
-    private void Init()
+    public void Init(DamageNumbersHandler pool)
     {
         _lifeTime = _damageNumberStats.LifeTime;
         _moveUpSpeed = _damageNumberStats.MoveUpSpeed;
-        
         _text = GetComponent<TextMeshPro>();
+        StartCoroutine(MoveAndReturn(pool));
     }
-    
+
     public void SetDamage(float damage)
     {
         _text.text = damage.ToString("0");
     }
 
-    private IEnumerator MoveAndDestroy()
+    private IEnumerator MoveAndReturn(DamageNumbersHandler pool)
     {
-        while (_lifeTime > 0)
+        float timeLeft = _lifeTime;
+        while (timeLeft > 0)
         {
-            transform.position += Vector3.up * (_moveUpSpeed * Time.fixedDeltaTime);
-            _lifeTime -= Time.fixedDeltaTime;
+            transform.position += Vector3.up * (_moveUpSpeed * Time.deltaTime);
+            timeLeft -= Time.deltaTime;
             yield return new WaitForFixedUpdate();
         }
-        Destroy(gameObject);
+        pool.AddToPool(this);
     }
 }
--- Assets/Scripts/UI/DamageNumbersHandler.cs
+++ Assets/Scripts/UI/DamageNumbersHandler.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class DamageNumbersHandler: MonoBehaviour
+public class DamageNumbersHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _damageNumberPrefab;
-    
     [SerializeField] private float _offset;
     private readonly float _offsetZ = -0.1f;
+
+    private readonly Queue<DamageNumber> _pool = new();
+    private Vector3 _spawnPosition;
 
     private void OnEnable()
     {
@@ -16,11 +19,38 @@ public class DamageNumbersHandler: MonoBehaviour
         Enemy.OnTakeDamage -= ShowDamageNumber;
     }
 
+    public void AddToPool(DamageNumber damageNumber)
+    {
+        damageNumber.gameObject.SetActive(false);
+        _pool.Enqueue(damageNumber);
+    }
+
+    private DamageNumber GetFromPool()
+    {
+        if (_pool.Count == 0)
+        {
+            return SpawnDamageNumber();
+        }
+
+        var damageNumber = _pool.Dequeue();
+        damageNumber.gameObject.SetActive(true);
+        damageNumber.transform.position = _spawnPosition;
+        return damageNumber;
+    }
+
+    private DamageNumber SpawnDamageNumber()
+    {
+        var damageNumber = Instantiate(_damageNumberPrefab, _spawnPosition, Quaternion.identity);
+        return damageNumber.GetComponent<DamageNumber>();
+    }
+
     private void ShowDamageNumber(float damage, Vector2 enemyPosition)
     {
-        float offsetX = Random.Range(-_offset, _offset);
-        var spawnPosition = new Vector3(enemyPosition.x + offsetX, enemyPosition.y, _offsetZ);
-        var damageNumber = Instantiate(_damageNumberPrefab, spawnPosition, Quaternion.identity);
-        damageNumber.GetComponent<DamageNumber>().SetDamage(damage);
+        var offsetX = Random.Range(-_offset, _offset);
+        _spawnPosition = new Vector3(enemyPosition.x + offsetX, enemyPosition.y, _offsetZ);
+        var damageNumber = GetFromPool();
+        damageNumber.transform.position = _spawnPosition;
+        damageNumber.Init(this);
+        damageNumber.SetDamage(damage);
     }
 }

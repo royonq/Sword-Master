fc24fcf170fa6180e50ab6b727c11b4a6df31b4f
Luxi4ka <vaivanto1403@gmail.com>
2025-05-25 11:43:12 +0200

    refactor

---
+++ Assets/Scripts/UI/DamageNumber.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class DamageNumber : MonoBehaviour
+{
+    [SerializeField] private DamageNumberStats _damageNumberStats;
+    
+    private  float _moveUpSpeed;
+    private  float _lifeTime;
+    
+    [SerializeField] private TextMeshPro _text;
+
+    private void Awake()
+    {
+        Init();
+        StartCoroutine(MoveAndDestroy());
+    }
+
+    private void Init()
+    {
+        _lifeTime = _damageNumberStats.LifeTime;
+        _moveUpSpeed = _damageNumberStats.MoveUpSpeed;
+    }
+    
+    public void SetDamage(float damage)
+    {
+        if (damage == 0)
+        {
+            throw new ArgumentException("Damage cannot be zero");
+        }
+        _text.text = damage.ToString("0");
+    }
+
+    private IEnumerator MoveAndDestroy()
+    {
+        while (_lifeTime > 0)
+        {
+            transform.position += Vector3.up * (_moveUpSpeed * Time.fixedDeltaTime);
+            _lifeTime -= Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        Destroy(gameObject);
+    }
+}

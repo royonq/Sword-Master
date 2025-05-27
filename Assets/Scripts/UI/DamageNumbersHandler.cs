dede1d9594373c0a7e448e072333b0f77896d8df
Luxi4ka <vaivanto1403@gmail.com>
2025-05-28 01:43:35 +0200

    refactor

--- Assets/Scripts/UI/DamageNumbersHandler.cs
+++ Assets/Scripts/UI/DamageNumbersHandler.cs
@@ -1,23 +1,26 @@
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class DamageNumbersHandler : MonoBehaviour
 {
-    [SerializeField] private DamageNumber _damageNumberPrefab;
+    [SerializeField] private GameObject _damageNumberPrefab;
+    [SerializeField] private DamageNumberStats _damageNumberStats;
     [SerializeField] private float _offset;
     private readonly int _minPoolSize = 20;
     private readonly int _maxPoolSize = 70;
     private readonly float _offsetZ = -0.1f;
 
-    private ObjectPool<DamageNumber> _pool;
+    private ObjectPool<GameObject> _pool;
 
     private void Awake()
     {
-        _pool = new ObjectPool<DamageNumber>(
+        _pool = new ObjectPool<GameObject>(
             createFunc: () => Instantiate(_damageNumberPrefab),
-            actionOnGet: damageNumber => damageNumber.gameObject.SetActive(true),
-            actionOnRelease: damageNumber => damageNumber.gameObject.SetActive(false),
-            actionOnDestroy: damageNumber => Destroy(damageNumber.gameObject),
+            actionOnGet: damageNumber => damageNumber.SetActive(true),
+            actionOnRelease: damageNumber => damageNumber.SetActive(false),
+            actionOnDestroy: Destroy,
             collectionCheck: false,
             defaultCapacity: _minPoolSize,
             maxSize: _maxPoolSize
@@ -27,26 +30,34 @@ public class DamageNumbersHandler : MonoBehaviour
     private void OnEnable()
     {
         Enemy.OnTakeDamage += ShowDamageNumber;
-        DamageNumber.OnShowDamage += AddToPool;
     }
 
     private void OnDisable()
     {
         Enemy.OnTakeDamage -= ShowDamageNumber;
-        DamageNumber.OnShowDamage -= AddToPool;
     }
-
-    private void AddToPool(DamageNumber damageNumber)
-    {
-        _pool.Release(damageNumber);
-    }
-
+    
     private void ShowDamageNumber(float damage, Vector2 enemyPosition)
     {
         var offsetX = Random.Range(-_offset, _offset);
         var spawnPosition = new Vector3(enemyPosition.x + offsetX, enemyPosition.y, _offsetZ);
 
         var damageNumber = _pool.Get();
-        damageNumber.Init(damage, spawnPosition);
+        var _text = damageNumber.GetComponent<TextMeshPro>();
+        _text.text = damage.ToString("0");
+        damageNumber.transform.position = spawnPosition;
+        StartCoroutine(MoveAndReturn(damageNumber));
+    }
+    
+    private IEnumerator MoveAndReturn(GameObject damageNumber)
+    {
+        var lifetime = _damageNumberStats.LifeTime;
+        while (lifetime > 0)
+        {
+            damageNumber.transform.position += Vector3.up * (_damageNumberStats.MoveUpSpeed * Time.fixedDeltaTime);
+            lifetime -= Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        _pool.Release(gameObject);
     }
 }

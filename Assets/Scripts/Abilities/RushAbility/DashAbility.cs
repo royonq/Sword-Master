dfa2a4560b8f0cc2f4a23cdfa4b49243a6082ac9
Luxi4ka <vaivanto1403@gmail.com>
2025-06-07 14:03:12 +0200

    changed line to trail

--- Assets/Scripts/Abilities/RushAbility/DashAbility.cs
+++ Assets/Scripts/Abilities/RushAbility/DashAbility.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using TMPro.Examples;
 using UnityEngine.InputSystem;
 
 public class DashAbility : Ability
@@ -7,18 +8,19 @@ public class DashAbility : Ability
     [SerializeField] private Camera _camera;
     private DashAbilityStats _dashAbilityStats;
     private Rigidbody2D _rb;
-    private LineRenderer _lineRenderer;
     private Player _player;
     
     private float _slowDuration;
     private float _slowFactor;
     private float _slowRadius;
+    
+    private TrailRenderer _trail;
 
     private void Awake()
     {
         _rb = GetComponentInParent<Rigidbody2D>();
         _player = GetComponentInParent<Player>();
-        _lineRenderer = GetComponentInParent<LineRenderer>();
+        _trail = GetComponentInParent<TrailRenderer>();
     }
 
     protected override void InitAbility()
@@ -35,21 +37,18 @@ public class DashAbility : Ability
     {
         _player.SetCanMove(false);
         _player.SetInvulnerable(true);
-        
+        _trail.enabled = true;
         var cursorWorldPos = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         var direction = ((Vector2)cursorWorldPos - (Vector2)transform.position).normalized;
 
         var start = _rb.position;
         var end = start + direction * dashDistance;
-
-        InitDashLine(start);
         
         float elapsed = 0;
         while (elapsed < dashDuration)
         {
             var cerrentPosition = Vector2.Lerp(start, end, elapsed / dashDuration);
             _rb.MovePosition(cerrentPosition);
-            _lineRenderer.SetPosition(1, cerrentPosition);
             
             elapsed += Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
@@ -57,13 +56,10 @@ public class DashAbility : Ability
 
         _rb.MovePosition(end);
         
-        _lineRenderer.SetPosition(1, end);
-        _lineRenderer.enabled = false;
-        
         HitEnemiesOnLine(start, end);
-        
         _player.SetCanMove(true);
         _player.SetInvulnerable(false);
+        _trail.enabled = false;
     }
     
     private void HitEnemiesOnLine(Vector2 start, Vector2 end)
@@ -93,12 +89,5 @@ public class DashAbility : Ability
         projection = Mathf.Clamp01(projection);
         var closestPoint = segmentStart + segment * projection;
         return Vector2.Distance(point, closestPoint);
-    }
-    
-    private void InitDashLine(Vector2 start)
-    {
-        _lineRenderer.enabled = true;
-        _lineRenderer.positionCount = 2;
-        _lineRenderer.SetPosition(0, start);
     }
 }

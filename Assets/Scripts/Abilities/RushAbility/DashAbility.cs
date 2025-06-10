b9f6638745edbe2c73f6c36a185e082015249332
Luxi4ka <vaivanto1403@gmail.com>
2025-06-10 22:57:57 +0200

    refactor

--- Assets/Scripts/Abilities/RushAbility/DashAbility.cs
+++ Assets/Scripts/Abilities/RushAbility/DashAbility.cs
@@ -13,9 +13,9 @@ public class DashAbility : Ability
     private float _slowDuration;
     private float _slowCoefficient;
     private float _slowRadius;
-    private float _dashDistance;
-    private float _dashDuration;
-    
+    private float _dashForce;
+    private readonly float _dashDuration = 1;
+
     private TrailRenderer _trail;
 
     
@@ -29,26 +29,25 @@ public class DashAbility : Ability
         _slowDuration = _dashAbilityStats.SlowDuration;
         _slowCoefficient = _dashAbilityStats.SlowCoefficient;
         _slowRadius = _dashAbilityStats.SlowRadius;
-        _dashDistance = _dashAbilityStats.Distance; 
-        _dashDuration = _dashAbilityStats.Duration;
+        _dashForce = _dashAbilityStats.Force; 
     }
 
     protected override void InitAbility()
     {
-        StartCoroutine(Dash(_dashDistance, _dashDuration));
+        StartCoroutine(Dash());
     }
 
     public override void UpgradeAbility(ItemsData itemData)
     {
         base.UpgradeAbility(itemData);
         var upgradeStats = itemData as UpgradeDashAbility;
-        _dashDistance += upgradeStats.IncreaseDistance;
+        _dashForce += upgradeStats.IncreaseDistance;
         _slowDuration += upgradeStats.IncreaseSlowDuration;
         _slowCoefficient += upgradeStats.IncreaseSlowCoefficient;
         _slowRadius += upgradeStats.IncreaseSlowRadius;
     }
 
-    private IEnumerator Dash(float dashDistance, float dashDuration)
+    private IEnumerator Dash()
 {
     _player.SetCanMove(false);
     _player.SetInvulnerable(true);
@@ -57,15 +56,14 @@ public class DashAbility : Ability
     var cursorWorldPos = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
     var direction = ((Vector2)cursorWorldPos - (Vector2)transform.position).normalized;
 
-    float dashSpeed = dashDistance / dashDuration;
-    _rb.AddForce(direction * dashSpeed, ForceMode2D.Impulse);
+    _rb.AddForce(direction * _dashForce, ForceMode2D.Impulse);
 
-    yield return new WaitForSeconds(dashDuration);
+    yield return new WaitForSeconds(_dashDuration);
 
     _rb.velocity = Vector2.zero;
     
     var start = _rb.position;
-    var end = _rb.position - direction * dashDistance;
+    var end = _rb.position - direction * _dashForce;
     
     if (_isAbilityUpgraded)
     {
@@ -104,7 +102,7 @@ public class DashAbility : Ability
         var cursorWorldPos = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         var direction = ((Vector2)cursorWorldPos - _rb.position).normalized;
         var boxSize = Vector2.one * _slowRadius;
-        float length = _dashAbilityStats.Distance;
+        float length = _dashAbilityStats.Force;
 
         Gizmos.color = Color.cyan;
         var oldMatrix = Gizmos.matrix;
